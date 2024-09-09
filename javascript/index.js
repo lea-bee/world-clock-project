70a2d3c54a07607730429d5384e5c9f77d086474
lea-bee <twigg.lt20@gmail.com>
2024-09-09 15:05:31 +0200

    Added the correct time through js

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+function updateTimes() {
+  //chicago
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
+
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTimes();
+setInterval(updateTimes, 1000);
